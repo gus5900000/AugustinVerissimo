384fe54addc931da7b07c173e3ecd4b9a41643c3
Augustin Verissimo <augustin.verissimo@epitech.eu>
2025-02-28 10:04:54 +0100

    remove console log

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js')
         .then(registration => {
-          console.log('Service Worker enregistré avec succès:', registration.scope);
+            return;
         })
         .catch(error => {
           console.error('Échec de l\'enregistrement du Service Worker:', error);
